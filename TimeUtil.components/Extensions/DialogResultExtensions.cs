cac426f873bc210486806c99d3c79a73fb82ca3d
devmcdev <97288649+devmcdev@users.noreply.github.com>
2023-01-04 13:33:08 +0000

    updated dependences

--- TimeUtil.components/Extensions/DialogResultExtensions.cs
+++ TimeUtil.components/Extensions/DialogResultExtensions.cs
@@ -6,7 +6,7 @@ internal static class DialogResultExtensions
 {
     public static bool TryGetSuccessfulResult<T>(this DialogResult dialogResult, [NotNullWhen(true)] out T? data)
     {
-        if (!dialogResult.Cancelled && dialogResult.Data is T value)
+        if (!dialogResult.Canceled && dialogResult.Data is T value)
         {
             data = value;
             return true;
